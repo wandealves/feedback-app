526d0abd72a159ce7882e9beb8f5c7270c20ae3a
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2021-12-26 14:55:57 -0300

    feat: adição do styles no compoenente Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 function App () {
   return (
     <>
-    <Header text="Feedbacl UI"/>
+    <Header/>
     <div className="container">
       <h1>App</h1>
     </div>
