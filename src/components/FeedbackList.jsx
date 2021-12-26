6ccb68100ed5a3ad6f30f42cb05965d57bfd5cd8
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2021-12-26 15:56:20 -0300

    feta: adição da funcionalidade de remover item

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,14 @@ import PropTypes from 'prop-types'
 
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList ({ feedback }) {
+function FeedbackList ({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-          <FeedbackItem key={item.id} item={item}/>
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
       ))}
     </div>
   )
@@ -22,7 +22,8 @@ FeedbackList.propTypes = {
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired
     })
-  )
+  ),
+  handleDelete: PropTypes.func
 }
 
 export default FeedbackList
