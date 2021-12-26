a4856c4e5be63cf9ad9867d8656b16fb30d28e1a
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2021-12-26 15:41:05 -0300

    feat: criação do componente Card e adição dos propTypes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList ({ feedback }) {
@@ -6,10 +8,20 @@ function FeedbackList ({ feedback }) {
   }
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
+      {feedback.map((item) => (
           <FeedbackItem key={item.id} item={item}/>
-      })}
+      ))}
     </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
   )
 }
 
