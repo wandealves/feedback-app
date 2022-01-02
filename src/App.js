0c01fa18f0a86bdcc593b6fc94d8b052b9abf024
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-01-02 11:49:31 -0300

    feat: funcionalidade de adicionar novo feedback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -10,6 +11,11 @@ import FeedbackData from './data/FeedbackData'
 function App () {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
   const deleteFeedback = (id) => {
     if (window.confirm('are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
@@ -20,7 +26,7 @@ function App () {
     <>
     <Header/>
     <div className="container">
-      <FeedbackForm/>
+      <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
 import Button from './shared/Button'
 
-function FeedbackForm () {
+function FeedbackForm ({ handleAdd }) {
   const [text, setText] = useState('')
   const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
@@ -24,9 +25,23 @@ function FeedbackForm () {
     setText(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating
+      }
+
+      handleAdd(newFeedback)
+
+      setText('')
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?
         </h2>
         <RatingSelect select={(rating) => setRating(rating)} />
@@ -38,6 +53,10 @@ function FeedbackForm () {
       </form>
     </Card>
   )
+}
+
+FeedbackForm.propTypes = {
+  handleAdd: PropTypes.func
 }
 
 export default FeedbackForm
