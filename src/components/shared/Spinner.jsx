778e7606f4c5c72b799e46e13610f763bc02e2fd
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-01-15 13:27:07 -0300

    feat: adição fetch get feedback e loading

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../assets/spinner.gif'
+
+function Spinner () {
+  return (
+    <img src={spinner} alt='Loading...' style={{
+      width: '100px', margin: 'auto', diplay: 'block'
+    }}/>
+  )
+}
+
+export default Spinner
