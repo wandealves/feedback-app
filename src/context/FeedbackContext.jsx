a33ddf99d91a1719c3ff3a45d3aecac68131ead7
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-01-15 13:39:06 -0300

    feat: adição fetch post

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -19,16 +18,25 @@ export const FeedbackProvider = ({ children }) => {
   // Fetch feedback
 
   const fetchfeedback = async () => {
-    const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+    const response = await fetch('/feedback?_sort=id&_order=desc')
     const data = await response.json()
 
     setFeedback(data)
     setIsLoading(false)
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
   }
 
   const editFeedback = (item) => {
